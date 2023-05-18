1ee1c4403a73198f0c6f4933b952cd77e6ccf164
HARIPRASAD P <67548227+Haripofficial@users.noreply.github.com>
2023-05-18 14:57:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def Add(a, b):
         return a + b
         
 def SayHello():
-        print("sup world from srcmake...")
+        print("sup world from srcmake.......")
 
 if __name__ == '__main__':
         SayHello()
