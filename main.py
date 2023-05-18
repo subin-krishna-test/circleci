d783c83c8c1d4884e2790096245e8c6c08c85871
subin-krishna-test <93134410+subin-krishna-test@users.noreply.github.com>
2023-05-18 15:05:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def Add(a, b):
         return a + b
         
 def SayHello():
-        print("sup world from srcmake.......")
+        print("sup world from srcmake........EF.E.")
 
 if __name__ == '__main__':
         SayHello()
